cdbab485435ae0f8c1e7cdbc2f6e5075cf1e2071
Mustafa-ismail-Eyi <eyi201201@gmail.com>
2020-04-29 14:42:44 +0300

    calistifakatfonksiyonlaringorevlerieklenmedi

--- shellscipt.c
+++ shellscipt.c
@@ -31,17 +31,17 @@ void read_command(char cmd[], char *par[]){
 }
 
 void type_prompt(){
-    static int first_time = 1;
+    /*static int first_time = 1;
     if (first_time){
         const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
         write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
         first_time = 0;
-    }
+    }*/
     printf("#"); //display prompt
 }
 
 
-int mian()
+int main()
 {
     char cmd[100], command[100], *parameters[20];
     // environment variable
